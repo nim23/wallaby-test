3d63cc77393f4b446be2a57526a1ddb4b79cd79b
Artem Govorov <artem.govorov@gmail.com>
2016-05-27 09:36:45 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,13 +1,12 @@
 'use strict'
 let wallabify = require('wallabify')
 let babel = require('babel')
-let wallabyPostProcessor = wallabify({})
+let wallabyPostProcessor = wallabify({ extensions: ['.js', '.jsx'] })
 
 module.exports = function (wallaby) {
   return {
     files: [
       {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
-      {pattern: 'src/utils/karma.polyfill.js', instrument: false},
       {pattern: 'src/**/*.json', instrument: false, load: false},
       {pattern: 'src/**/*.js*', load: false},
       {pattern: 'src/**/*.spec.js', ignore: true}
